ea91dffb189301fa2f07ad81e24305e9e66e8017
abdur7 <abdulrehman.319@gmail.com>
2024-01-23 18:09:23 +0530

    made connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.DBConnect)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 const app = express();
 
 const PORT = 3000;
