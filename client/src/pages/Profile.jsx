09d66a565023e2bd4f27eaf90350cccfe653af39
abdur7 <abdulrehman.319@gmail.com>
2024-02-08 13:18:50 +0530

    delete listing complete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -22,6 +22,7 @@ export default function Profile() {
     const [updateSuccess, setUpdateSuccess] = useState(false)
     const [userListings, setUserListings] = useState([])
     const [showListingsError, setShowListingsError] = useState(null)
+    const [deleteListingError, setDeleteListingError] = useState(false)
 
 
     const fileRef = useRef(null)
@@ -151,6 +152,25 @@ export default function Profile() {
 
     }
 
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, {
+                method: "DELETE"
+            })
+
+            const data = res.json()
+
+            if (data.success === false) {
+                setDeleteListingError(data.message)
+                return
+            }
+
+            setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -208,7 +228,7 @@ export default function Profile() {
                             </Link>
 
                             <div className='flex flex-col items-center'>
-                                <button className='uppercase text-red-700'>Delete</button>
+                                <button onClick={() => handleListingDelete(listing._id)} className='uppercase text-red-700'>Delete</button>
                                 <button className='uppercase text-green-700'>Edit</button>
                             </div>
 
