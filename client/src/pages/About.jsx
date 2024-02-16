647468a7a32d1ddce813e11732ff8b4ad65d3ede
abdur7 <abdulrehman.319@gmail.com>
2024-02-16 19:01:34 +0530

    About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>About</div>
+        <div className='py-20 px-4 mx-auto max-w-6xl'>
+            <h1 className='font-bold text-3xl mb-4 text-slate-800'>About ARKEstate</h1>
+            <p className='mb-4 text-slate-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Ut amet animi praesentium vitae officiis earum reiciendis accusamus cum laudantium cumque quo,
+                magnam sequi architecto eveniet aspernatur omnis voluptate dignissimos recusandae molestiae neque
+                quisquam obcaecati minus! Dicta, tempora voluptatibus aliquam ullam impedit, dolores illo debitis
+                quibusdam laudantium sint maxime veniam autem quasi optio sequi quam officia amet tenetur perferendis consequatur,
+                accusantium laboriosam at? Quam ab aliquam sunt amet sequi odit voluptatem molestiae eum minus, et quaerat eaque reiciendis maiores? Qui, dicta!
+            </p>
+
+            <p className='mb-4 text-slate-700'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Doloremque dolorem delectus nobis vel repellat debitis ea magni quisquam accusantium
+                natus modi optio, quia minima repellendus hic repudiandae. Neque facilis cum ex ratione?
+                Quidem non labore ad magnam qui quisquam ea id beatae! Distinctio fugit dignissimos numquam
+                ut architecto, aut dolores repellendus impedit vero nostrum quaerat quod animi, nemo facere veritatis?
+            </p>
+
+            <p className="mb-4 text-slate-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo saepe, dolorum quasi a quis
+                accusantium accusamus eligendi quas culpa, earum alias cumque iste sit commodi corporis temporibus
+                ad quibusdam nemo magni vero quam recusandae amet nisi magnam! Magnam quae deserunt, repellat, cupiditate
+                fugiat rem dolore quasi labore distinctio, error maxime? Libero sed eveniet maxime perferendis. Sed, illum
+                quis beatae corrupti corporis, voluptate hic tempora, sint maxime dolorem quia neque. Iste tenetur natus aliquam
+                vel quae laudantium ad quibusdam, excepturi molestias.
+            </p>
+        </div>
     )
 }
